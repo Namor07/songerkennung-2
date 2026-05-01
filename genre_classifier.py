66d891616ea0c934a63e37846d4662f1a130c8df
Namor07 <romanhunzinger@gmail.com>
2026-05-01 18:53:24 +0200

    Update genre_classifier.py

--- genre_classifier.py
+++ genre_classifier.py
@@ -2,25 +2,42 @@ import numpy as np
 import librosa
 import tensorflow as tf
 
-# Modell & Labels laden
-model = tf.keras.models.load_model("soundclassifier_with_metadata.tflite")
+MODEL_PATH = "tflite_model/soundclassifier_with_metadata.tflite"
 
-with open("genre_model/labels.txt", "r") as f:
+# Interpreter laden
+interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+# Labels laden
+with open("tflite_model/labels.txt", "r") as f:
     LABELS = [line.strip() for line in f.readlines()]
 
 
-def extract_features(audio_file):
-    y, sr = librosa.load(audio_file, sr=22050, mono=True)
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
-    mfcc = np.mean(mfcc.T, axis=0)
-    return mfcc
+def preprocess_audio(audio_file):
+    # 1 Sekunde, 16 kHz, Mono
+    y, sr = librosa.load(audio_file, sr=16000, mono=True)
+
+    if len(y) < 16000:
+        y = np.pad(y, (0, 16000 - len(y)))
+    else:
+        y = y[:16000]
+
+    y = y.astype(np.float32)
+    y = np.expand_dims(y, axis=0)
+
+    return y
 
 
 def predict_genre(audio_file):
-    features = extract_features(audio_file)
-    features = features.reshape(1, -1)
+    audio = preprocess_audio(audio_file)
 
-    prediction = model.predict(features)
-    index = np.argmax(prediction)
+    interpreter.set_tensor(input_details[0]["index"], audio)
+    interpreter.invoke()
+
+    output = interpreter.get_tensor(output_details[0]["index"])[0]
+    index = int(np.argmax(output))
 
     return LABELS[index]
