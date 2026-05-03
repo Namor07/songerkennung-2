2c8938052644b965a5bdba8ad55e371f5edd16be
Namor07 <romanhunzinger@gmail.com>
2026-05-03 19:12:00 +0200

    Refactor genre classifier for improved input handling

--- genre_classifier.py
+++ genre_classifier.py
@@ -1,31 +1,31 @@
-import io
 import numpy as np
 import librosa
-import soundfile as sf
 import tensorflow as tf
-from scipy.signal import resample
 
-MODEL_PATH = "tflite_model/soundclassifier_with_metadata.tflite"
+MODEL_PATH = "soundclassifier_with_metadata.tflite"
 
-# TensorFlow Lite Interpreter
-interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+CLASS_NAMES = [
+    "Hintergrundgeräusche",
+    "Klassiche Musik",
+    "Pop",
+    "Rock",
+]
+
+interpreter = tf.lite.Interpreter(
+    model_path=MODEL_PATH,
+    experimental_delegates=[]
+)
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Erwartete Audio-Länge aus Modell lesen
-EXPECTED_SAMPLES = input_details[0]["shape"][1]
+INPUT_LENGTH = input_details[0]["shape"][1]
 
-# Labels laden
-with open("tflite_model/labels.txt", "r") as f:
-    LABELS = [line.strip() for line in f.readlines()]
-
-
-def extract_segments(audio_file, sr=16000, segment_duration=5, num_segments=5):
+def extract_segments(audio_file, sr=16000, num_segments=5):
     y, sr = librosa.load(audio_file, sr=sr, mono=True)
 
-    segment_length = sr * segment_duration
+    segment_length = INPUT_LENGTH
     max_start = max(0, len(y) - segment_length)
 
     segments = []
@@ -41,17 +41,14 @@ def extract_segments(audio_file, sr=16000, segment_duration=5, num_segments=5):
 
     return segments
 
-
 def predict_genre(audio_file):
     segments = extract_segments(audio_file)
-
     predictions = []
 
     for segment in segments:
-        input_data = segment.reshape(1, -1)
+        input_data = segment.reshape(1, INPUT_LENGTH)
         interpreter.set_tensor(input_details[0]["index"], input_data)
         interpreter.invoke()
-
         output = interpreter.get_tensor(output_details[0]["index"])[0]
         predictions.append(output)
 
